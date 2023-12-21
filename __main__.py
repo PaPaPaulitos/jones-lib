d44e13c4ba07a6569495a75683141a7b237ba903
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-12-21 13:03:47 -0300

    Search in fake data

--- __main__.py
+++ __main__.py
@@ -1,7 +1,12 @@
-from controllers import ReadFile, DirectoryScanner
+from controllers import ReadFile, DirectoryScanner, FilterBreachedData
 
 
 
 read_file = ReadFile('./files/fake_data.csv')
 data = read_file.read()
-print(data)
+
+filter_breached_data = FilterBreachedData()
+
+filtered_data = filter_breached_data.filter_breached_data(data)
+
+print(filtered_data)
--- controllers/etl_file.py
+++ controllers/etl_file.py
@@ -15,5 +15,11 @@ class FilterBreachedData:
             new_dict_data.append(dict_output)
 
         return new_dict_data
+    
+    def search_in_filter_data(self,dict_data:list,username:str,email:str):
+        for i in dict_data:
+            if i['username'] == username or i['email'] == email:
+                return True
+        return False
 
         
